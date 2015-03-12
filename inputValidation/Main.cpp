bad3c9dcc5d731b7cfb8f9f02a41102a68d956c9
BrandonErb <berb-cc@conestogac.on.ca>
2015-03-12 12:43:20 -0400

    Adding the test harness

---
+++ inputValidation/Main.cpp
@@ -0,0 +1,9 @@
+//This is the test harness for the program
+
+int main(void) 
+{ 
+	inputValidation ob; 
+	char *name = "Kitchener123"; 
+	ob.validateCity(name); 
+	return 0; 
+}
--- inputValidation/inputValidation.c
+++ inputValidation/inputValidation.c
@@ -1,9 +1,1 @@
 #include "inputValidation.h"
-
-
-int main()
-{
-
-	printf("Enter an address\n");
-	return 0;
-}
